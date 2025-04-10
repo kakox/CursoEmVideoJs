fbadde2f50385af916c5efa60d35b24dd2fcc140
Kauan Ribeiro <kauanribeirobravo@gmail.com>
2025-04-10 01:32:28 -0300

    implementando gerador de tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,15 @@
-alert('Olá')
+function contar(){
+    // DOM
+    var n = Number(window.document.getElementById('num').value)
+    var res = document.getElementById('res')
+    // verifica se o n é igual a 0, caso for faz o n valer 1 para fazer a tabuada do 1 
+    if (n === 0){
+        alert('Número inválido, considerando número 1')
+        n = 1
+    }
+    // laço para fazer a tabuada
+    res.innerHTML = ''
+    for( var i = 1; i <=10; i++ ){
+      res.innerHTML +=`${n} x ${i} = ${n*i} <br>`
+    }
+}
